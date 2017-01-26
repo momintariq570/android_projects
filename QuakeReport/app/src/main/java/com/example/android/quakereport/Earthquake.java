7620a29a1094597c52897ab404fcec844308be84
Momin Tariq <momintariq@Momins-MBP.fios-router.home>
2017-01-25 21:08:30 -0500

    Udacity nanodegree

--- QuakeReport/app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ QuakeReport/app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -8,12 +8,14 @@ public class Earthquake {
 
     private double mMagnitude;
     private String mLocation;
-    private String mDate;
+    private long mTimeInMilliseconds;
+    private String mURL;
 
-    public Earthquake(double magnitude, String location, String date) {
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url) {
         mMagnitude = magnitude;
         mLocation = location;
-        mDate = date;
+        mTimeInMilliseconds = timeInMilliseconds;
+        mURL = url;
     }
 
     public double getMagnitude() {
@@ -24,7 +26,11 @@ public class Earthquake {
         return mLocation;
     }
 
-    public String getDate() {
-        return mDate;
+    public long getTimeInMilliseconds() {
+        return mTimeInMilliseconds;
+    }
+
+    public String getURL() {
+        return mURL;
     }
 }
--- QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -1,13 +1,19 @@
 package com.example.android.quakereport;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
+import android.net.Uri;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by momintariq on 12/30/16.
@@ -27,17 +33,108 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
                     R.layout.list_item, parent, false);
         }
 
-        Earthquake currentEarthquake = getItem(position);
+        final Earthquake currentEarthquake = getItem(position);
 
         TextView magnitude = (TextView)listItemView.findViewById(R.id.list_item_magnitude);
         magnitude.setText(Double.toString(currentEarthquake.getMagnitude()));
 
+        GradientDrawable magnitudeCircle = (GradientDrawable)magnitude.getBackground();
+        int magnitudeColor = getMagnitudeColor(currentEarthquake.getMagnitude());
+        magnitudeCircle.setColor(magnitudeColor);
+
+        String fullLocation = currentEarthquake.getLocation();
+        String[] locationArr = locationItems(fullLocation);
+
+        TextView locationOffset = (TextView)listItemView.findViewById(R.id.list_item_location_offset);
+        locationOffset.setText(locationArr[0]);
+
         TextView city = (TextView)listItemView.findViewById(R.id.list_item_city);
-        city.setText(currentEarthquake.getLocation());
+        city.setText(locationArr[1]);
+
+        Date dateObject = new Date(currentEarthquake.getTimeInMilliseconds());
 
         TextView date = (TextView)listItemView.findViewById(R.id.list_item_date);
-        date.setText(currentEarthquake.getDate());
+        String formattedDate = formatDate(dateObject);
+        date.setText(formattedDate);
+
+        TextView time = (TextView)listItemView.findViewById(R.id.list_item_time);
+        String formattedTime = formatTime(dateObject);
+        time.setText(formattedTime);
+
+        listItemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = currentEarthquake.getURL();
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                getContext().startActivity(i);
+            }
+        });
 
         return listItemView;
+    }
+
+    private String formatDate(Date dateObj) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("LLL dd, yyyy");
+        return dateFormat.format(dateObj);
+    }
+
+    private String formatTime(Date dateObj) {
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+        return timeFormat.format(dateObj);
+    }
+
+    private String[] locationItems(String location) {
+
+        if(location.contains("of")) {
+            String[] outputArr = location.split("of");
+            outputArr[0] = outputArr[0] + "of";
+            return outputArr;
+        } else {
+            String[] outputArr = new String[2];
+            outputArr[0] = "Near the";
+            outputArr[1] = location;
+            return outputArr;
+        }
+    }
+
+    private int getMagnitudeColor(double magnitude) {
+        int magnitudeColorResourceId;
+        int magnitudeFloor = (int)Math.floor(magnitude);
+
+        switch(magnitudeFloor) {
+            case 0:
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
     }
 }
